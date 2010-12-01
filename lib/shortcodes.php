9a3848b611cd71fc567a0fa2ddb64dab1875ec55
Nductiv <Nductiv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-01 20:48:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/3d-pix/tags/trunk@317724 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/shortcodes.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @author Tony Asch
+ * @copyright 2010
+ * @description Use WordPress Shortcode API for more features
+ * @Docs http://codex.wordpress.org/Shortcode_API
+ */
+
+class tdPix_shortcodes {
+	
+	var $count = 1;
+	
+	// register the new shortcodes
+	function tdPix_shortcodes() {
+	
+		add_shortcode( '3dpix', array(&$this, 'show_tdPix') );
+			
+	}
+
+	
+	function show_tdPix( $atts ) {
+	
+		global $tdPix;
+	
+		extract(shortcode_atts(array(
+			'id' 		=> false,
+			'width'  => get_option('tdpix_width'),
+			'height' => get_option('tdpix_height'),
+			'mode' => get_option('tdpix_mode')
+		), $atts ));
+		
+		$out = '<span><iframe class="phereoframe"  src="http://phereo.com/a/embed/' . $id . '/' . $mode . '/' . $width . '.' . $height . '/important" width="' . $width . '" height="' . $height . '" frameborder="1" vspace="0" hspace="0" marginwidth="0" marginheight="0" scrolling="no" noresize><p class="pherotagline">See stereo 3D on <a href="http://phereo.com">Phereo</a>.</p></iframe></span>';
+			
+		return $out;
+	}
+
+	
+}
+
+// let's use it
+$tdPixShortcodes = new tdPix_Shortcodes;	
+
+?>
+
